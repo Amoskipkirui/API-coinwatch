7bdf0a2640b0e71593677d7c945ebccd5b958c82
Amos Kipkirui <emohkipkirui756@gmail.com>
2023-09-05 14:57:30 +0300

    API

---
+++ api.py
@@ -0,0 +1,30 @@
+
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': '4d0fdbeb-c941-4ae7-a0e0-c7296f124313',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+
+import pandas as pd
+
+#JSON normalize
+pd.json_normalize(data['data'])
